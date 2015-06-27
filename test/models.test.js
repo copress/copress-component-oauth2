20e82b4c637e23d22294a6cfc92e3c07f4d8dfe3
taoyuan <torworx@gmail.com>
2015-06-27 16:06:38 +0800

    Initial commit

---
+++ test/models.test.js
@@ -0,0 +1,28 @@
+var syclify = require('syclify');
+var t = require('chai').assert;
+var modelsBuilder = require('../lib/models');
+
+describe('sycle-component-oauth2/models', function () {
+
+    it('should work', function (done) {
+        //this.timeout(2000);
+        var p = __dirname + '/..';
+        var sapp = syclify({
+            modules: [p],
+            db: {
+                autoupdate: true,
+                driver: 'mysql',
+                collation: 'utf8_unicode_ci',
+                database: 'oauth_test',
+                username: 'root'
+            }
+        });
+        sapp.boot(function (err) {
+            if (err) throw err;
+
+            modelsBuilder(app);
+
+            done();
+        })
+    });
+});
